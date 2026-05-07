449ee1691fb41ac50721534e9d7916c5e475be07
thangcx <canxuanthang2001tt@gmail.com>
2026-05-07 16:35:11 +0700

    fix: useGetLocalStorage

--- src/hook/useGetLocalStorage.ts
+++ src/hook/useGetLocalStorage.ts
@@ -5,7 +5,9 @@ export default function useGetLocalStorage<T>(key: string) {
     if (typeof window === "undefined") return {} as T;
 
     const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : ({} as T);
+    return data !== null && data.length > 0 && data !== "undefined"
+      ? JSON.parse(data)
+      : ({} as T);
   });
 
   return storedValue;
